bf59960b22b85fdd7974a6b0b3797c56c5696958
yns0ng <phplaber@gmail.com>
2024-04-14 08:57:13 +0800

    信息收集工具支持端口扫描

--- getinfo.py
+++ getinfo.py
@@ -7,7 +7,9 @@ import time
 import socket
 import optparse
 import requests
+import nmap
 import dns.resolver
+from tabulate import tabulate
 from urllib.parse import urlparse, unquote
 
 # 忽略 SSL 告警信息
@@ -143,7 +145,7 @@ if __name__ == '__main__':
     """
     信息收集
 
-    1、基本信息；2、SSL 证书；3、DNS 记录；4、杂项
+    1、基本信息；2、端口信息；3、SSL 证书；4、DNS 记录；5、杂项
     """
 
     # 基本信息
@@ -191,6 +193,27 @@ Web 服务软件：{bcolors.BOLD + web_server + bcolors.ENDC}
 框架/脚本语言：{bcolors.BOLD + framework + bcolors.ENDC}
     """
     print(basic_info)
+
+    # 端口信息
+    print(f'{"-"*10} 端口信息 {"-"*10}')
+    print('\n')
+    ports_info = []
+    nm = nmap.PortScanner()
+    nm.scan(domain)
+    for host in nm.all_hosts():
+        print(f'主机 IP：{host}')
+        #print(f'状态：{nm[host].state()}')
+        for proto in nm[host].all_protocols():
+            if proto not in ["tcp", "udp"]:
+                continue
+
+            lport = list(nm[host][proto].keys())
+            lport.sort()
+            for pt in lport:
+                ports_info.append([f'{pt}/{proto}', nm[host][proto][pt]["state"], nm[host][proto][pt]["name"], f'{nm[host][proto][pt]["product"]} {nm[host][proto][pt]["version"]}'])
+    
+    print(tabulate(ports_info, headers=['端口', '状态', '服务', '版本'], tablefmt='simple_grid'))
+    print('\n')
 
     # SSL 证书信息
     print(f'{"-"*10} SSL 证书信息 {"-"*10}')
